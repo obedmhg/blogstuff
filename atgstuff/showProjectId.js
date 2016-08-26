5cc31108b9e8501ec8b44dbd9f040214345a7c57
Obed Murillo <obedmhg@gmail.com>
2016-08-26 13:39:42 -0600

    Create showProjectId.js

---
+++ atgstuff/showProjectId.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name          Append ProjectId BCC
+// @description   It will appen the project Id to the project name.
+// @version       0.0.1
+// @include       /atg/
+// @author        Obed Murillo
+// @require http://code.jquery.com/jquery-latest.js
+// @grant         none
+// ==/UserScript==
+
+(function ($, undefined) {
+  $(function () {
+      $('a').each(function() {
+          if (this.href.indexOf("project=") >= 0) {
+              var afterProject = this.href.split("project=");
+              var projectId = afterProject[1].split("&")[0];
+              $(this).after( "<div style='border: 1px solid white; background-color: #00AC10; color: white; width: 65px; height: 12px;  text-align: center; vertical-align: middle;line-height: 12px; '>"+projectId+" </div>" );
+          }
+      });
+  });
+})(window.jQuery.noConflict(true));
